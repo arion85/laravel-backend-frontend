2834276e0d25ef6bccf6af174b938cbb0e01562f
Evgeniy Arioshkin <arionservice@gmail.com>
2018-08-10 11:49:59 +0300

    chg config including

--- Backend/config/filesystems.php
+++ Backend/config/filesystems.php
@@ -8,7 +8,7 @@ $file_name = basename(__FILE__);
 
 $common_config = realpath(app()->basePath().'/../Common/config/'.$file_name);
 if(is_file($common_config)){
-    return array_merge_recursive($return_array, include_once ($common_config));
+    return array_merge_recursive($return_array, include ($common_config));
 }
 
 return $return_array;
