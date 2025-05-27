d2c7a9ab0fc893f27cee4ec089d0df1fb49ff2db
evgeniy <arioshkin.es@gmail.com>
2025-05-27 09:18:43 +0300

    Separeted start Backend and Frontend
    sss

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,45 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\ApplicationBuilder;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
+const DS = DIRECTORY_SEPARATOR;
+
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();
+
+$app->beforeBootstrapping('Illuminate\Foundation\Bootstrap\BootProviders', function (Application $app){
+    $request = app('request');
+    $config = app('config');
+
+    $adm_prefURL = $config->get('app.app_admin_prefixurl');
+
+    if (preg_match("/^{$adm_prefURL}.*/", $request->host())) {
+        $side = 'backend';
+    }else{
+        $side = 'frontend';
+    }
+
+    $app->instance('path.side',$app->basePath($side));
+    $app->instance('path.resources',$app->get('path.side').DS.'resources');
+
+    $app->get(ApplicationBuilder::class)
+        ->withRouting(
+            web: $app->get('path.side').DS.'routes'.DS.'web.php',
+            commands: $app->get('path.side').DS.'routes'.DS.'console.php',
+            health: '/up',
+
+        );
+    $view = app('view');
+    $view->addLocation(app()->get('path.side').DS.'views');
+});
+
+return $app;
