9ac09951500cb90fdb7f7ed3fd85bd19db0d570b
evgeniy <arioshkin.es@gmail.com>
2025-05-28 20:24:03 +0300

    Fix namespaces for Backend and Frontend

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Backend\App\Http\Controllers;
 
 abstract class Controller
 {
--- frontend/app/Http/Controllers/Controller.php
+++ frontend/app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Frontend\App\Http\Controllers;
 
 abstract class Controller
 {
