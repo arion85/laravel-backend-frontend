639e643817b023c789e754b12530153fabf96010
evgeniy <arioshkin.es@gmail.com>
2025-05-29 20:49:32 +0300

    Separated Tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\Request;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected string $side;
+    public function createApplication()
+    {
+        array_push($_SERVER['argv'], $this->side);
+        ++$_SERVER['argc'];
+
+        $app = require Application::inferBasePath().'/common/bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
+
