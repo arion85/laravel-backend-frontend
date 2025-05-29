fcc21c0f6a043753f25a1df173c0b1eea5fe3adb
evgeniy <arioshkin.es@gmail.com>
2025-05-29 10:48:14 +0300

    Fix Artisan

--- frontend/app/Models/User.php
+++ frontend/app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<\common\database\factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
