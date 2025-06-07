7ca1def933a54429a769f0bad0ceec3ff0a5b256
evgeniy <arioshkin.es@gmail.com>
2025-06-07 18:35:49 +0300

    Separated Back|Common|Front Providers

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $a=5;
     }
 
     /**
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $a=5;
     }
 }
